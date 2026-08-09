6bd0bc71f6f44672d0a22da4048ac4b6105dcad0
soemasao <z@soemasao.com>
2026-08-09 12:50:28 +0700

    chore: set chrome page load strategy to eager for faster check

--- src/main.py
+++ src/main.py
@@ -156,6 +156,7 @@ async def _perform_domain_check(domain_names_list, username, update: Update, con
         chrome_options.add_argument("--window-size=1920,1080") 
         chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
         chrome_options.add_argument("--log-level=3") 
+        chrome_options.page_load_strategy = 'eager'
         
         log_message(username, "Menginisialisasi browser Chrome...")
         
